5d14f85afcb32213f201bf56a7221b76b76e507f
paulz <zhufengminpaul@163.com>
2015-12-29 17:22:28 +0800

    add comment for data format

--- sys/Constraint.h
+++ sys/Constraint.h
@@ -5,18 +5,25 @@
 
 class Constraint {
 public:
-    const static int NOT_NULL = 0;
-    const static int UNIQUE = 1;
-    const static int PRIMARY_KEY = 2;
-    const static int FOREIGN_KEY = 3;
-    const static int CHECK = 4;
-    const static int DEFAULT = 5;
-    const static int AUTO_INCREMENT = 6;
+    const static int NOT_NULL = 0; // data is empty
+    const static int UNIQUE = 1; // data is empty
+    const static int PRIMARY_KEY = 2; // data is empty
+    const static int FOREIGN_KEY = 3; // data shows the referenced column:
+                                      // { "tb": table, "col": column }
+    const static int CHECK = 4; // data shows the BoolExpr
+                                // when used, calling
+                                //     Tree* toTreeNode(const rapidjson::Value& val);
+                                // in parser/Tree.h to unserialze to type BoolExpr
+    const static int DEFAULT = 5; // data shows the default Value
+                                  // when used, calling
+                                  //     Tree* toTreeNode(const rapidjson::Value& val);
+                                  // in parser/Tree.h to unserialze to type Value
+    const static int AUTO_INCREMENT = 6; // data is empty
 
 	short cid;		// reference to column id
-	string name;
-	char type;
-	rapidjson::Value data;
+	string name;    // column name
+	char type;      // which type
+	rapidjson::Value data; // see the comments after each type
 
 	Constraint();
 	Constraint(short cid, const string& name, char type,
